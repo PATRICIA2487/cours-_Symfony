6cbe8707d0d5489251aceed0ee880ea32c2385d6
PATRICIA2487 <155555017+PATRICIA2487@users.noreply.github.com>
2024-05-07 11:08:27 +0200

    cours_Symfony

--- src/Controller/ProductFormController.php
+++ src/Controller/ProductFormController.php
@@ -4,17 +4,27 @@ namespace App\Controller;
 
 use App\Entity\Products;
 use App\Form\ProductType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ProductFormController extends AbstractController
 {
     #[Route('/product/form', name: 'app_product_form')]
-    public function index(): Response
+    public function index(Request $request, EntityManagerInterface $entityManagerInterface): Response
     {
         $product = new Products();
         $form = $this->createForm(ProductType::class, $product);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $entityManagerInterface->persist($product);
+            $entityManagerInterface->flush();
+
+            return $this->redirectToRoute('app_product_form');
+        }
 
         return $this->render('product_form/index.html.twig', [
             'form' => $form,
--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -4,18 +4,30 @@ namespace App\Form;
 
 use App\Entity\Products;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Length;
 
 class ProductType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('ProductName')
-            ->add('productPrice')
+            ->add('ProductName', TextType::class, [
+                'label' => "Nom du produit",
+                'constraints' => [
+                        new Length(['min' => 5, 'max' => 20])
+                    ]
+            ])
+            ->add('productPrice',)
             ->add('productType')
             ->add('productDesc')
+            ->add('submit', SubmitType::class, [
+                'label' => "Confirmer",
+                'attr' => ['class'=> "btn btn-success"]
+            ])
         ;
     }
 
