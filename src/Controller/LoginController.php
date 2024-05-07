c279df598129201f2a7428fda0e06ba307337e89
PATRICIA2487 <155555017+PATRICIA2487@users.noreply.github.com>
2024-05-07 16:43:51 +0200

    cours_Symfony

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -10,7 +10,6 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 class LoginController extends AbstractController
 {
     #[Route('/login', name: 'app_login')]
-    
     public function index(AuthenticationUtils $authenticationUtils): Response
         {
             // get the login error if there is one
@@ -24,5 +23,11 @@ class LoginController extends AbstractController
                 'last_username' => $lastUsername,
                 'error'         => $error,
             ]);
+        }
+
+    #[Route(path: '/logout', name: 'app_logout')]
+    public function logout(): void
+        {
+            throw new \LogicException('This method can be blank - it will be intercepted by the logout key on your firewall.');
         }
 }
