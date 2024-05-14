818f01aa74c1327bcacc90268326ee6026b10ef7
PATRICIA2487 <155555017+PATRICIA2487@users.noreply.github.com>
2024-05-14 09:43:26 +0200

    cours_symfony

---
+++ src/Controller/ProfileController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Security\Http\Attribute\IsGranted;
+
+class ProfileController extends AbstractController
+{
+    // #[IsGranted('IS_AUTHENTICATED')]
+    #[Route('/profile', name: 'app_profile')]
+    public function index(): Response
+    {
+        return $this->render('profile/index.html.twig', [
+            'controller_name' => 'ProfileController',
+        ]);
+    }
+}
